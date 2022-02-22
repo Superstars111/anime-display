0faa3737cdf4e42005b57d4527ee680bf59f5cea
Superstars111 <jared.superstars111@gmail.com>
2022-02-21 16:10:09 -0800

    Fixed library collection

--- builder.py
+++ builder.py
@@ -4,190 +4,12 @@ from flask import render_template
 import json
 import pandas as pd
 
-# download_data = pd.read_json("http://superstars111.pythonanywhere.com/library", typ="series", orient="records")
-# mild_warnings = download_data[0]
-# extreme_warnings = download_data[1]
-# library = download_data[2]
-
-# with open("anime_data.json", "r") as anime_data:
-#     full_data = json.load(anime_data)
-
-full_data = pd.read_json("anime_data.json", typ="series", orient="records")
+try:
+    full_data = pd.read_json("anime_data.json", typ="series", orient="records")
+except ValueError:
+    full_data = pd.read_json("/home/Superstars111/mysite/anime_data.json", typ="series", orient="records")
 
 library = full_data[2]
-
-# library = [{
-#             "id": "20755",
-#             "romajiTitle": "Ansatsu Kyoushitsu",
-#             "englishTitle": "Assassination Classroom",
-#             "nativeTitle": "\u6697\u6bba\u6559\u5ba4",
-#             "defaultTitle": "Assassination Classroom",
-#             "format": "TV",
-#             "description": "The students of class 3-E have a mission: kill their teacher before graduation. He has already destroyed the moon, and has promised to destroy the Earth if he can not be killed within a year. But how can this class of misfits kill a tentacled monster, capable of reaching Mach 20 speed, who may be the best teacher any of them have ever had?",
-#             "episodes": 47,
-#             "seasons": 2,
-#             "unairedSeasons": 0,
-#             "movies": 0,
-#             "coverLarge": "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx20755-q0b3Ok1cAbPd.jpg",
-#             "coverMed": "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx20755-q0b3Ok1cAbPd.jpg",
-#             "coverSmall": "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx20755-q0b3Ok1cAbPd.jpg",
-#             "genres": [
-#                 "Action",
-#                 "Comedy",
-#                 "Drama",
-#                 "Supernatural"
-#             ],
-#             "tags": [
-#                 {
-#                     "name": "Assassins",
-#                     "rank": 95,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "School",
-#                     "rank": 93,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Teacher",
-#                     "rank": 88,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Cultivation",
-#                     "rank": 86,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Shounen",
-#                     "rank": 79,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Ensemble Cast",
-#                     "rank": 75,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Super Power",
-#                     "rank": 67,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Anti-Hero",
-#                     "rank": 64,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Crime",
-#                     "rank": 61,
-#                     "isMediaSpoiler": True
-#                 },
-#                 {
-#                     "name": "Bullying",
-#                     "rank": 61,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Male Protagonist",
-#                     "rank": 56,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Guns",
-#                     "rank": 56,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Primarily Teen Cast",
-#                     "rank": 50,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Artificial Intelligence",
-#                     "rank": 48,
-#                     "isMediaSpoiler": True
-#                 },
-#                 {
-#                     "name": "Episodic",
-#                     "rank": 43,
-#                     "isMediaSpoiler": False
-#                 },
-#                 {
-#                     "name": "Crossdressing",
-#                     "rank": 41,
-#                     "isMediaSpoiler": True
-#                 },
-#                 {
-#                     "name": "Yandere",
-#                     "rank": 26,
-#                     "isMediaSpoiler": True
-#                 }
-#             ],
-#             "score": 80,
-#             "streaming": {
-#                 "crunchyroll": {
-#                     "seasons": 0,
-#                     "movies": 0
-#                 },
-#                 "funimation": {
-#                     "seasons": 2,
-#                     "movies": 0
-#                 },
-#                 "prison": {
-#                     "seasons": 2,
-#                     "movies": 0
-#                 },
-#                 "amazon": {
-#                     "seasons": 0,
-#                     "movies": 0
-#                 },
-#                 "vrv": {
-#                     "seasons": 0,
-#                     "movies": 0
-#                 },
-#                 "hulu": {
-#                     "seasons": 2,
-#                     "movies": 0
-#                 },
-#                 "youtube": {
-#                     "seasons": 1,
-#                     "movies": 0
-#                 },
-#                 "tubi": {
-#                     "seasons": 0,
-#                     "movies": 0
-#                 },
-#                 "hbo": {
-#                     "seasons": 0,
-#                     "movies": 0
-#                 },
-#                 "hidive": {
-#                     "seasons": 0,
-#                     "movies": 0
-#                 }
-#             },
-#             "houseScores": [
-#                 [
-#                     "Jared",
-#                     93,
-#                     25,
-#                     20
-#                 ],
-#                 [
-#                     "Simon",
-#                     95,
-#                     15,
-#                     25
-#                 ],
-#                 [
-#                     "Kenan",
-#                     70,
-#                     -10,
-#                     25
-#                 ]
-#             ]
-#         }]
 
 app = Flask(__name__)
 
@@ -198,13 +20,13 @@ def build_webpage():
     if id:
         show = find_show(id)
         title = collect_title(show)
-        # image = collect_image(show)
-        # episodes = collect_episodes(show)
-        # seasons = collect_seasons(show)
-        # movies = collect_movies(show)
-        # unaired = collect_unaired(show)
-        # synopsis = collect_synopsis(show)
-        # public_score = collect_public_score(show)
+        image = collect_image(show)
+        episodes = collect_episodes(show)
+        seasons = collect_seasons(show)
+        movies = collect_movies(show)
+        unaired = collect_unaired(show)
+        synopsis = collect_synopsis(show)
+        public_score = collect_public_score(show)
         # private_score = collect_private_score(show)
         # graph = collect_graph(show)
         # genres = collect_genres(show)
@@ -213,9 +35,34 @@ def build_webpage():
         # spoilers = collect_spoilers(show)
         # streaming = collect_streaming(show)
     else:
-        title = "Assassination Classroom"
+        title = "Title"
+        image = "Image"
+        episodes = "Episodes"
+        seasons = "Seasons"
+        movies = "Movies"
+        unaired = "Unaired"
+        synopsis = "Synopsis"
+        public_score = "Public"
+        # private_score = collect_private_score(show)
+        # graph = collect_graph(show)
+        # genres = collect_genres(show)
+        # tags = collect_tags(show)
+        # warnings = collect_warnings(show)
+        # spoilers = collect_spoilers(show)
+        # streaming = collect_streaming(show)
 
-    return render_template("home.html", title=title)
+    variables = {
+        "title": title,
+        "image": image,
+        "episodes": episodes,
+        "seasons": seasons,
+        "movies": movies,
+        "unaired": unaired,
+        "synopsis": synopsis,
+        "public": public_score,
+    }
+
+    return render_template("home.html", **variables)
 
 
 def find_show(id):
@@ -229,31 +76,31 @@ def collect_title(show):
 
 
 def collect_image(show):
-    pass
+    return f"{show['coverMed']}"
 
 
 def collect_episodes(show):
-    pass
+    return f"{show['episodes']}"
 
 
 def collect_seasons(show):
-    pass
+    return f"{show['seasons']}"
 
 
 def collect_movies(show):
-    pass
+    return f"{show['movies']}"
 
 
 def collect_unaired(show):
-    pass
+    return f"{show['unairedSeasons']}"
 
 
 def collect_synopsis(show):
-    pass
+    return show['description']
 
 
 def collect_public_score(show):
-    pass
+    return f"{show['score']}"
 
 
 def collect_private_score(show):
@@ -285,5 +132,4 @@ def collect_streaming(show):
 
 
 if __name__ == "__main__":
-
     app.run(host="127.0.0.1", port=8080, debug=True)
