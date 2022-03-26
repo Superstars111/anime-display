da46c67d68b9fde361c4d81aa742b892eb4e79a4
Superstars111 <jared.superstars111@gmail.com>
2022-03-25 18:02:11 -0700

    Added registration process

--- project/auth.py
+++ project/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import UserModel
 from . import db
 
 auth = Blueprint("auth", __name__)
@@ -17,6 +19,21 @@ def login():
     #         return redirect("/display_all")
 
     return render_template("login.html")
+
+
+@auth.route("/login", methods=["POST"])
+def login_post():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    remember = True if request.form.get("remember") else False
+
+    user = UserModel.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash("Your login details don't match. Please try again.")
+        return redirect(url_for("auth.login"))
+
+    return redirect(url_for("main.profile"))
 
 
 @auth.route("/register")
@@ -39,6 +56,27 @@ def register():
     #     return redirect("/login")
 
     return render_template("register.html")
+
+
+@auth.route("/register", methods=["POST"])
+def register_post():
+
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    user = UserModel.query.filter_by(email=email).first()
+
+    if user:
+        flash("This email is already in use")
+        return redirect(url_for("auth.register"))
+
+    new_user = UserModel(email=email, username=username, password=generate_password_hash(password, method="sha256"))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for("auth.login"))
 
 
 @auth.route("/logout")
--- project/models.py
+++ project/models.py
@@ -1,5 +1,6 @@
 # Taken from https://www.askpython.com/python-modules/flask/flask-user-authentication
 # and from https://realpython.com/python-sqlite-sqlalchemy/
+# and from https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login
 
 from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
@@ -7,8 +8,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, LoginManager
+from . import db
 
-db = SQLAlchemy()
 login = LoginManager()
 
 
@@ -18,7 +19,7 @@ class UserModel(UserMixin, db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
     username = db.Column(db.String(100))
-    password_hash = db.Column(db.String())
+    password_hash = db.Column(db.String(100))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
