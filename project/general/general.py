104e304c24e79b15521ddabe963c4ceffd16e26f
Superstars111 <jared.superstars111@gmail.com>
2022-05-12 16:05:41 -0700

    Added update tracker

--- project/general/general.py
+++ project/general/general.py
@@ -10,6 +10,7 @@ from project.config import settings
 import json
 
 general = Blueprint("general", __name__, template_folder="../../project")
+template_path = "general/templates/general"
 # app = Flask(__name__)
 # app.secret_key = "testing"
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///anime-display-users.db"
@@ -32,32 +33,9 @@ def index():
         return redirect(url_for("auth.login"))
 
 
-@general.route("/graph_test", methods=["POST", "GET"])
-def graph_test():
-    data = [
-        {"x": 2, "y": 6},
-        {"x": 7, "y": -9},
-        {"x": -10, "y": -12},
-        {"x": 37, "y": 38},
-        {"x": -50, "y": -50},
-        {"x": 50, "y": 50}
-    ]
-    change = request.args.get("change", "")
-    if change:
-        data = [
-            {"x": 23, "y": 40},
-            {"x": 32, "y": -23},
-            {"x": -30, "y": -10},
-            {"x": 2, "y": 3},
-            {"x": 50, "y": -50},
-            {"x": -50, "y": 50}
-        ]
-        data = json.dumps(data)
-        return data
-    else:
-        pass
-    data = json.dumps(data)
-    return render_template("graph_test.html", data_test=data)
+@general.route("/updates")
+def update_log():
+    return render_template(f"{template_path}/updates.html")
 
 
 @general.errorhandler(404)
@@ -66,48 +44,32 @@ def error404(error):
     <a href="/display">Go back</a>""", 404
 
 
-# def collect_image(show):
-#     return f"{show['coverMed']}"
-#
-#
-# def collect_episodes(show):
-#     return f"{show['episodes']}"
-#
-#
-# def collect_seasons(show):
-#     return f"{show['seasons']}"
-#
-#
-# def collect_movies(show):
-#     return f"{show['movies']}"
-#
-#
-# def collect_unaired(show):
-#     return f"{show['unairedSeasons']}"
-#
-#
-# def collect_synopsis(show):
-#     return show['description']
-#
-#
-# def collect_public_score(show):
-#     return f"{show['score']}"
-
-
-def collect_genres(show):
-    pass
-
-
-def collect_tags(show):
-    pass
-
-
-def collect_warnings(show):
-    pass
-
-
-def collect_spoilers(show):
-    pass
+# @general.route("/graph_test", methods=["POST", "GET"])
+# def graph_test():
+#     data = [
+#         {"x": 2, "y": 6},
+#         {"x": 7, "y": -9},
+#         {"x": -10, "y": -12},
+#         {"x": 37, "y": 38},
+#         {"x": -50, "y": -50},
+#         {"x": 50, "y": 50}
+#     ]
+#     change = request.args.get("change", "")
+#     if change:
+#         data = [
+#             {"x": 23, "y": 40},
+#             {"x": 32, "y": -23},
+#             {"x": -30, "y": -10},
+#             {"x": 2, "y": 3},
+#             {"x": 50, "y": -50},
+#             {"x": -50, "y": 50}
+#         ]
+#         data = json.dumps(data)
+#         return data
+#     else:
+#         pass
+#     data = json.dumps(data)
+#     return render_template("graph_test.html", data_test=data)
 
 
 if __name__ == "__main__":
